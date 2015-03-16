8d20b95d9ebd324518a0b7ef05b35904527dcab1
Kevin Haire <kevhaire@gmail.com>
2015-03-16 11:44:57 -0400

    changed redirect for shop sites

--- inc/_redirect.php
+++ inc/_redirect.php
@@ -1,4 +1,12 @@
 <?php
+
+	$referer = $_SERVER["HTTP_REFERER"];
+	$url = $_SERVER["HTTP_HOST"];
+
+	if (strpos($referer,'shop') !== false || strpos($referer, $url) !== false) {
+		$redirect = "false";
+	}
+
 	if ($redirect == "true") {
 		$iphone  = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
 		$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
@@ -6,7 +14,7 @@
 		$berry   = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
 		$ipod    = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
 
-		if ($iphone || $android || $palmpre || $ipod || $berry == true) 
+		if ($iphone || $android || $palmpre || $ipod || $berry == true)
 		{
 		    echo "<script>window.location='http://www.".$shopDomain."/mobile/index.cfm/redirect/".$bizId."'</script>";
 		}
